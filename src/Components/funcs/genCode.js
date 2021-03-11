ef45b6628b14ff1e473889c93bbc641e84c6a03f
technicolour24k <35193590+technicolour24k@users.noreply.github.com>
2021-03-11 13:30:51 +0000

    Added gearswap file section
    - Updated code generator to include "spell/action" variables
    - Updated snippets section
    - Updated gearswap collective section
    - Added section to "create" a gearswap file
    - Updated action bar to allow user to specify a gearset

--- src/Components/funcs/genCode.js
+++ src/Components/funcs/genCode.js
@@ -1,7 +1,7 @@
-export function addToSnippets(selectedOption, optionValue) {
+export function addToSnippets(selectedOption, optionValue, equipSet) {
     document.getElementById("genSnippets").innerHTML = "";
     let arr = [`if (${selectedOption} == '${optionValue}') then`,
-    `Your Code Goes Here!`,
+    `equip(${equipSet})`,
     `end`];
     arr.forEach(element => {
         let node = document.createElement("p");
@@ -11,14 +11,26 @@ export function addToSnippets(selectedOption, optionValue) {
     });
 }
 
-export function addToGearswap(selectedOption, optionValue) {
+export function addToGearswapCollective(selectedOption, optionValue, equipSet) {
     let arr = [`if (${selectedOption} == '${optionValue}') then`,
-    `Your Code Goes Here!`,
+    `equip(${equipSet})`,
     `end`];
     arr.forEach(element => {
         let node = document.createElement("p");
         let textnode = document.createTextNode(element);
         node.appendChild(textnode);
         document.getElementById("genCode").appendChild(node);
+    });
+}
+
+export function addToGearswapFile(section, selectedOption, optionValue, equipSet) {
+    let arr = [`if (${selectedOption} == '${optionValue}') then`,
+    `equip(${equipSet})`,
+    `end`];
+    arr.forEach(element => {
+        let node = document.createElement("p");
+        let textnode = document.createTextNode(element);
+        node.appendChild(textnode);
+        document.getElementById(`genFile${section}`).appendChild(node);
     });
 }
